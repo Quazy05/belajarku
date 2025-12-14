a4dedaacf9e0b9a83613025a3b47741ed76b6c62
AbyHakim <155170498+Quazy05@users.noreply.github.com>
2025-12-14 15:12:13 +0700

    Delete my-node-project directory

--- my-node-project/src/controllers/index.js
+++
@@ -1,9 +0,0 @@
-export const getData = (req, res) => {
-    // Logic to retrieve data
-    res.send("Data retrieved successfully");
-};
-
-export const postData = (req, res) => {
-    // Logic to handle data submission
-    res.send("Data submitted successfully");
-};
--- my-node-project/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getData, postData } = require('../controllers');
-
-const router = express.Router();
-
-router.get('/data', getData);
-router.post('/data', postData);
-
-module.exports = router;
